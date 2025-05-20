c029f2668c99bf97fcebafe6313dc2d1b73699ed
Henry Mayta <lpze.henrybladimir.mayta.ca@unifranz.edu.bo>
2025-05-20 18:08:54 -0400

    Primer commit: proyecto Angular practicaclase13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
-import { ImageService } from './services/image.service';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,10 +12,9 @@ import { Injectable } from '@angular/core';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
-  providers: [ImageService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/image-gallery/image-gallery.component.ts
+++ src/app/components/image-gallery/image-gallery.component.ts
@@ -1,59 +1,19 @@
-import { Component } from '@angular/core';
-import { ImageService } from 'src/app/services/image.service';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { RickAndMortyService } from '../../services/rick-and-morty.service';
 
 @Component({
   selector: 'app-image-gallery',
   templateUrl: './image-gallery.component.html',
   styleUrls: ['./image-gallery.component.css']
 })
-export class ImageGalleryComponent {
-  images: any[] = [];
-  isLoanding: boolean = true;
-  selectedImage: any = null;
-  error: string | null = null;
+export class ImageGalleryComponent implements OnInit {
+  personajes: any[] = [];
 
-  // configuración de la galería
-  itemsPerPage: number = 10;
-  currentPage: number = 1;
-
-  constructor(private imageService: ImageService) {}
+  constructor(private rmService: RickAndMortyService) {}
 
   ngOnInit(): void {
-    this.loadImages();
-  }
-  loadImages(): void {
-    this.isLoanding = true;
-    this.error = null;
-
-    this.imageService.getImages(this.itemsPerPage).subscribe({
-      next: (data): void => {
-        this.images = data;
-        this.isLoanding = false;
-      },
-      error: (error): void => {
-        this.error = 'Error loading images';
-        this.isLoanding = false;
-        console.error('Error loading images', error);
-      }
+    this.rmService.getPersonajes().subscribe(data => {
+      this.personajes = data.results;
     });
   }
-
-  showImageDetails(id: string): void {
-  this.imageService.getImageDetails(id).subscribe({
-    next: (details): void => {
-      this.selectedImage = details;
-    },
-    error: (err): void => {
-      console.error(err);
-    }
-  });
-}
-closemodal(): void {
-  this.selectedImage = null;
-}
-
 }
